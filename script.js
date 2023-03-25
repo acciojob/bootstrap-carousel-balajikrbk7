123a35b8b122e6f65c5a725471b61d0ff35e0d8b
Balaji K R <86788000+balajikrbk7@users.noreply.github.com>
2023-03-25 21:10:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your code here
+<!-- Your HTML code for the carousel goes here -->
+
+<!-- Link your Bootstrap JS file (must be placed after your HTML code) -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+<script>
+$(document).ready(function(){
+  // Activate the carousel
+  $("#myCarousel").carousel();
+  
+  // Enable carousel controls
+  $(".left").click(function(){
+    $("#myCarousel").carousel("prev");
+  });
+  $(".right").click(function(){
+    $("#myCarousel").carousel("next");
+  });
+});
+</script>
